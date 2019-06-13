1cf39e2ffc58e0d036d398bad9b3cb7faeabb356
Isaac Legred <inl7@cornell.edu>
2019-06-13 13:31:37 -0700

    Add Spectre Wrapper version of GenerateXdmf.py and vector implementation.

--- tools/SpGenerateXdmf.py
+++ tools/SpGenerateXdmf.py
@@ -4,7 +4,8 @@
 # See LICENSE.txt for details.
 
 import glob
-import h5py
+from spectre import DataStructures
+from spectre.IO import H5 as spectre_h5
 
 
 def generate_xdmf(file_prefix, output_filename, start_time, stop_time, stride):
@@ -12,13 +13,14 @@ def generate_xdmf(file_prefix, output_filename, start_time, stop_time, stride):
     Generate one XDMF file that ParaView and VisIt can use to load the data
     out of the HDF5 files.
     """
-    h5files = [(h5py.File(filename, 'r'), filename)
+    h5files = [(spectre_h5.H5File(filename, 1), filename)
                for filename in glob.glob(file_prefix + "*.h5")]
 
-    element_data = h5files[0][0].get('element_data.vol')
-    temporal_ids_and_values = [(x,
-                                element_data.get(x).attrs['observation_value'])
-                               for x in element_data.keys()]
+    element_data = h5files[0][0].get_vol('/element_data')
+    temporal_ids_and_values = \
+    [(observation_id, element_data.get_observation_value(
+        observation_id)) for observation_id in \
+     element_data.list_observation_ids()]
     temporal_ids_and_values.sort(key=lambda x: x[1])
 
     xdmf_output = "<?xml version=\"1.0\" ?>\n" \
@@ -40,25 +42,29 @@ def generate_xdmf(file_prefix, output_filename, start_time, stop_time, stride):
         if (stride_counter - 1) % stride != 0:
             continue
 
-        h5temporal = element_data.get(id_and_value[0])
         xdmf_output += "  <Grid Name=\"Grids\" GridType=\"Collection\">\n"
         xdmf_output += "    <Time Value=\"%1.14e\"/>\n" % (id_and_value[1])
         # loop over each h5 file
+        # Can't use quite the same structure here in the wrapper class as
+        # The wrapper classes don't have objects representing observation_id's,
+        # So we'll need to hold on to the .vol file, and get at the grid
+        # And the extents one at a time.
         for h5file in h5files:
-            h5temporal = h5file[0].get('element_data.vol').get(id_and_value[0])
-            for grid in h5temporal.keys():
-                extents = h5temporal.get(grid).attrs['extents']
+            current_vol = h5file[0].get_vol('/element_data')
+            h5temporal_id = id_and_value[0]
+            for grid in current_vol.list_grids(h5temporal_id):
+                extents = current_vol.get_extents(h5temporal_id, grid)
                 number_of_cells = 1
                 for x in extents:
                     number_of_cells *= (x - 1)
                 data_item = "        <DataItem Dimensions=\"%d %d %d\" " \
                     "NumberType=\"Double\" Precision=\"8\" Format=\"HDF5\">\n" \
                     % (extents[0], extents[1], extents[2])
-                data_item_vec = "        <DataItem Dimensions=\"%d %d %d 3\" "\
-                    "ItemType = \"Function\" Function = \"JOIN($0,$1,$2)\">\n"\
+                data_item_vec = "        <DataItem Dimensions=\"%d %d %d 3\" " \
+                "ItemType = \"Function\" Function = \"JOIN($0,$1,$2)\">\n" \
                     % (extents[0], extents[1], extents[2])
-                element_path = "          %s:/element_data.vol/%s/%s" % (
-                    h5file[1], id_and_value[0], grid)
+                element_path = "          %s:/element_data.vol/" %(h5file[1]) +\
+                "ObservationId%s/%s" % (id_and_value[0], grid)
                 xdmf_output += \
                     "    <Grid Name=\"%s\" GrideType=\"Uniform\">\n" % (grid)
                 # Write topology information
@@ -79,11 +85,12 @@ def generate_xdmf(file_prefix, output_filename, start_time, stop_time, stride):
                     "/InertialCoordinates_z\n        </DataItem>\n"
                 xdmf_output += "      </Geometry>\n"
                 # Write tensor components as scalars
-                components = list(h5temporal.get(grid).keys())
+                # Except for vectors, which can be written as vectors.
+                components = current_vol.list_tensor_components(
+                    h5temporal_id, grid)
                 components.remove('InertialCoordinates_x')
                 components.remove('InertialCoordinates_y')
                 components.remove('InertialCoordinates_z')
-                components.remove('connectivity')
                 for component in components:
                     # If the component is x component of a vector, write a
                     # vector for all three components
@@ -114,6 +121,7 @@ def generate_xdmf(file_prefix, output_filename, start_time, stop_time, stride):
                     else:
                         continue
                 xdmf_output += "    </Grid>\n"
+
 
         # close time grid
         xdmf_output += "  </Grid>\n"
