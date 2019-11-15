5699203cb6fd39f475fb13544cf3288ef535563c
Isaac Legred <inl7@cornell.edu>
2019-11-15 09:00:21 -0800

    remove uncessecary arguments in the time printing actions

--- src/IO/Observer/ReductionActions.hpp
+++ src/IO/Observer/ReductionActions.hpp
@@ -175,39 +175,15 @@ struct ContributeTimeInfo {
         [
           &observation_id, reduction_data = std::move(reduction_data),
           &reduction_names, &cache, &subfile_name
-        ](const gsl::not_null<std::unordered_map<
-              ObservationId, Parallel::ReductionData<Ts...>>*>
-              reduction_data_map,
-          const gsl::not_null<
-              std::unordered_map<ObservationId, std::vector<std::string>>*>
-              reduction_names_map,
-          const gsl::not_null<
+        ]( const gsl::not_null<
               std::unordered_map<observers::ObservationId, size_t>*>
               reduction_observers_contributed,
           const std::unordered_set<ArrayComponentId>&
               reduction_component_ids) mutable noexcept {
           auto& contribute_count =
               (*reduction_observers_contributed)[observation_id];
-          if (reduction_data_map->count(observation_id) == 0) {
-            reduction_data_map->emplace(observation_id,
-                                        std::move(reduction_data));
-            reduction_names_map->emplace(observation_id, reduction_names);
-            contribute_count = 1;
-          } else {
-            ASSERT(
-                reduction_names_map->at(observation_id) == reduction_names,
-                "Reduction names differ at ObservationId "
-                    << observation_id
-                    << " with the expected names being "
-                    // Use MakeString to get around ADL for STL stream operators
-                    // (MakeString is in global namespace).
-                    << (MakeString{} << reduction_names_map->at(observation_id)
-                                     << " and the received names being "
-                                     << reduction_names));
-            reduction_data_map->operator[](observation_id)
-                .combine(std::move(reduction_data));
-            contribute_count++;
-          }
+          contribute_count++;
+
 
           // Check if we have received all reduction data from the registered
           // elements. If so, we reduce to the local ObserverWriter nodegroup.
@@ -217,12 +193,7 @@ struct ContributeTimeInfo {
                                       ObserverWriter<Metavariables>>(cache)
                                       .ckLocalBranch();
             Parallel::threaded_action<ThreadedActions::PrintTimeInfo>(
-                local_writer, observation_id, subfile_name,
-                node_id == 0 ? std::move((*reduction_names_map)[observation_id])
-                             : std::vector<std::string>{},
-                std::move((*reduction_data_map)[observation_id]));
-            reduction_data_map->erase(observation_id);
-            reduction_names_map->erase(observation_id);
+                local_writer, observation_id);
             reduction_observers_contributed->erase(observation_id);
           }
         },
@@ -438,11 +409,7 @@ struct PrintTimeInfo {
                     Parallel::ConstGlobalCache<Metavariables>& cache,
                     const ArrayIndex& /*array_index*/,
                     const gsl::not_null<CmiNodeLock*> node_lock,
-                    const observers::ObservationId& observation_id,
-                    const std::string& subfile_name,
-                    std::vector<std::string>&& reduction_names,
-                    Parallel::ReductionData<ReductionDatums...>&&
-                        in_reduction_data) noexcept {
+                    const observers::ObservationId& observation_id) noexcept {
     CmiNodeLock file_lock;
     bool write_to_disk = false;
     std::vector<std::string> legend{};
@@ -473,16 +440,10 @@ struct PrintTimeInfo {
         make_not_null(&box),
         [
           &cache, &expected_calls_from_this_node,
-          &expected_calls_from_other_nodes, &file_lock, &in_reduction_data,
-          &legend, &observation_id, &reduction_names, &subfile_name,
+          &expected_calls_from_other_nodes, &file_lock,
+          &observation_id,
           &write_to_disk
         ](const gsl::not_null<
-              db::item_type<Tags::ReductionData<ReductionDatums...>>*>
-              reduction_data,
-          const gsl::not_null<
-              std::unordered_map<ObservationId, std::vector<std::string>>*>
-              reduction_names_map,
-          const gsl::not_null<
               std::unordered_map<observers::ObservationId, size_t>*>
               reduction_observers_contributed,
           const gsl::not_null<CmiNodeLock*> reduction_file_lock) noexcept {
@@ -490,26 +451,13 @@ struct PrintTimeInfo {
               (*reduction_observers_contributed)[observation_id];
           const auto node_id = Parallel::my_node();
 
-          if (node_id == 0 and not reduction_names.empty()) {
-            reduction_names_map->emplace(observation_id,
-                                         std::move(reduction_names));
-          }
-
           if (UNLIKELY(node_id == 0 and expected_calls_from_other_nodes == 0 and
                        expected_calls_from_this_node == 1)) {
             // Here this Action will be called only once, so we take
             // a shortcut and just write to disk.
             write_to_disk = true;
             file_lock = *reduction_file_lock;
-            legend = std::move(reduction_names_map->operator[](observation_id));
-            reduction_names_map->erase(observation_id);
-          } else if (reduction_data->count(observation_id) == 0) {
-            // This Action has been called for the first time,
-            // so all we need to do is move the input data to the
-            // reduction_data in the DataBox.
-            reduction_data->operator[](observation_id) =
-                std::move(in_reduction_data);
-            contribute_count = 1;
+
           } else if (node_id == 0 and
                      contribute_count == expected_calls_from_this_node +
                                              expected_calls_from_other_nodes -
@@ -519,19 +467,12 @@ struct PrintTimeInfo {
             // step, contribute_count (which was incremented at the end of each
             // previous call) should be one less than the expected number of
             // calls.
-            in_reduction_data.combine(
-                std::move(reduction_data->operator[](observation_id)));
-            reduction_data->erase(observation_id);
             reduction_observers_contributed->erase(observation_id);
             write_to_disk = true;
             file_lock = *reduction_file_lock;
-            legend = std::move(reduction_names_map->operator[](observation_id));
-            reduction_names_map->erase(observation_id);
           } else {
             // This Action is being called at least the second time
             // (but not the final time if on node 0).
-            reduction_data->at(observation_id)
-                .combine(std::move(in_reduction_data));
             contribute_count++;
           }
 
@@ -543,17 +484,14 @@ struct PrintTimeInfo {
             Parallel::threaded_action<PrintTimeInfo>(
                 Parallel::get_parallel_component<ObserverWriter<Metavariables>>(
                     cache)[0],
-                observation_id, subfile_name, std::vector<std::string>{},
-                std::move(reduction_data->operator[](observation_id)));
+                observation_id);
             reduction_observers_contributed->erase(observation_id);
-            reduction_data->erase(observation_id);
           }
         });
     Parallel::unlock(node_lock);
 
     if (write_to_disk) {
       Parallel::lock(&file_lock);
-      in_reduction_data.finalize();
       PrintTimeInfo::write_data(
           observation_id,
           std::make_index_sequence<sizeof...(ReductionDatums)>{});
