e0c7949a5c27d78c140d2541f1fb1c00d1c87a36
Isaac Legred <inl7@cornell.edu>
2020-06-09 09:35:31 -0700

    Add Basis and Quadrature extensions to python bindings

--- src/DataStructures/Python/Tensor/TensorData.cpp
+++ src/DataStructures/Python/Tensor/TensorData.cpp
@@ -1,16 +1,18 @@
 // Distributed under the MIT License.
 // See LICENSE.txt for details.
 
-#include <pybind11/operators.h>
+#include "DataStructures/Tensor/TensorData.hpp"
+#include <memory>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include <sstream>
 #include <string>
-#include <vector>
+#include <utility>
 
 #include "DataStructures/DataVector.hpp"
-#include "DataStructures/Tensor/TensorData.hpp"
+#include "NumericalAlgorithms/Spectral/Spectral.hpp"
+#include "PythonBindings/BoundChecks.hpp"
 #include "Utilities/GetOutput.hpp"
-#include "Utilities/StdHelpers.hpp"
 
 namespace py = pybind11;
 
@@ -20,29 +22,71 @@ void bind_tensordata(py::module& m) {  // NOLINT
   py::class_<TensorComponent>(m, "TensorComponent")
       .def(py::init<std::string, DataVector>(), py::arg("name"),
            py::arg("data"))
-      .def_readwrite("name", &TensorComponent::name)
-      .def_readwrite("data", &TensorComponent::data)
-      .def("__str__", get_output<TensorComponent>)
-      .def("__repr__", get_output<TensorComponent>)
-      // NOLINTNEXTLINE(misc-redundant-expression)
-      .def(py::self == py::self)
-      // NOLINTNEXTLINE(misc-redundant-expression)
-      .def(py::self != py::self);
+      .def("set_name",
+           [](TensorComponent& tc, const std::string& n) { tc.name = n; })
+      .def("get_name", [](const TensorComponent& tc) { return tc.name; })
+      .def("set_data",
+           [](TensorComponent& tc, const DataVector& d) { tc.data = d; })
+      .def("get_data", [](const TensorComponent& tc) { return tc.data; })
+      .def("__str__", [](const TensorComponent& tc) { return get_output(tc); })
+      .def("__repr__",
+           [](const TensorComponent& tc) { return get_output(tc); });
 
-  // Wrapper for ExtentsAndTensorVolumeData
-  py::class_<ExtentsAndTensorVolumeData>(m, "ExtentsAndTensorVolumeData")
-      .def(py::init<std::vector<size_t>, std::vector<TensorComponent>>(),
-           py::arg("extents"), py::arg("components"))
-      .def_readwrite("extents", &ExtentsAndTensorVolumeData::extents)
-      .def_readwrite("tensor_components",
-                     &ExtentsAndTensorVolumeData::tensor_components)
-      .def("__str__",
-           [](const ExtentsAndTensorVolumeData& extents_and_data) {
-    return "(" + get_output(extents_and_data.extents) + "," +
-           get_output(extents_and_data.tensor_components) + ")";
+  py::enum_<Spectral::Basis>(m, "SpectralBasis")
+    .value("Legendre", Spectral::Basis::Legendre)
+    .value("Chebyshev", Spectral::Basis::Chebyshev)
+    .value("FiniteDifference", Spectral::Basis::FiniteDifference)
+    .export_values();
+  py::enum_<Spectral::Quadrature>(m, "SpectralQuadrature")
+    .value("Gauss", Spectral::Quadrature::Gauss)
+    .value("GaussLobatto", Spectral::Quadrature::GaussLobatto)
+    .value("CellCentered", Spectral::Quadrature::CellCentered)
+    .value("FaceCentered", Spectral::Quadrature::FaceCentered)
+    .export_values();
+      // Wrapper for ExtentsAndTensorVolumeData
+      py::class_<ExtentsAndTensorVolumeData>(m, "ExtentsAndTensorVolumeData")
+          .def(py::init<std::vector<size_t>, std::vector<TensorComponent>>(),
+               py::arg("extents"), py::arg("components"))
+          .def("get_extents",
+               [](const ExtentsAndTensorVolumeData& etvd) {
+                 return etvd.extents;
+               })
+          .def("set_extents",
+               [](ExtentsAndTensorVolumeData& etvd,
+                  const std::vector<size_t>& v) { etvd.extents = v; })
+          .def("set_tensor_components",
+               [](ExtentsAndTensorVolumeData& etvd,
+                  const std::vector<TensorComponent>& v_tc) {
+                 etvd.tensor_components = v_tc;
+               })
+          .def("get_tensor_components",
+               [](const ExtentsAndTensorVolumeData& etvd) {
+                 return etvd.tensor_components;
+               });
+
+  py::class_<ElementVolumeData, ExtentsAndTensorVolumeData>(m,
+                                                            "ElementVolumeData")
+      .def(py::init<std::vector<size_t>, std::vector<TensorComponent>,
+                    std::vector<Spectral::Basis>,
+                    std::vector<Spectral::Quadrature>>(),
+           py::arg("extents"), py::arg("components"), py::arg("basis"),
+           py::arg("quadrature"))
+      .def("get_extents",
+           [](const ElementVolumeData& evd) { return evd.extents; })
+      .def("set_extents",
+           [](ElementVolumeData& evd, const std::vector<size_t>& v) {
+             evd.extents = v;
            })
-      .def("__repr__", [](const ExtentsAndTensorVolumeData& extents_and_data) {
-    return "(" + get_output(extents_and_data.extents) + "," +
-      get_output(extents_and_data.tensor_components) + ")";});
+      .def(
+          "set_tensor_components",
+          [](ElementVolumeData& evd, const std::vector<TensorComponent>& v_tc) {
+            evd.tensor_components = v_tc;
+          })
+      .def("get_tensor_components",
+           [](const ElementVolumeData& evd) { return evd.tensor_components; })
+    .def("get_basis", [](const ElementVolumeData& evd) { return evd.basis; })
+      .def("get_quadrature",
+           [](const ElementVolumeData& evd) { return evd.quadrature; });
 }
-} // namespace py_bindings
+
+}  // namespace py_bindings
