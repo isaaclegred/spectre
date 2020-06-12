184f0bd43d8c60f0ab29fbcfd8e3d6ee79b670f5
Isaac Legred <inl7@cornell.edu>
2020-06-12 15:32:07 -0700

    Add py arg and remove old file --patched

--- tests/Unit/IO/Test_VolumeData.py
+++ tests/Unit/IO/Test_VolumeData.py
@@ -27,6 +27,76 @@ class TestIOH5VolumeData(unittest.TestCase):
         if os.path.isfile(self.file_name_w):
             os.remove(self.file_name_w)
 
+        if os.path.isfile(self.file_name_r):
+            os.remove(self.file_name_r)
+
+        h5_file = spectre_h5.H5File(file_name=self.file_name_r,
+                                    append_to_file=True)
+        tensor_components_and_coords = [
+            ds.DataVector([8.9, 7.6, 3.9, 2.1, 18.9, 17.6, 13.9, 12.1]),
+            ds.DataVector([0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0]),
+            ds.DataVector([0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0]),
+            ds.DataVector([0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0]),
+            ds.DataVector([-78.9, -7.6, -1.9, 1.9, 6.3, 1.7, 9.8, 1.2]),
+            ds.DataVector([-7.9, 17.6, 1.9, -8.1, -6.3, 21.7, 6.8, -0.2]),
+            ds.DataVector([1.0, 6.6, 28.0, -8.1, -26.3, 32.7, 26.8, -3.2])
+        ]
+        observation_ids = [8435087234, 6785087234]
+        observation_values = [7.0, 1.3]
+        grid_names = ["[[2,3,4]]", "[[5,6,7]]"]
+        # Insert .vol file to h5 file
+        h5_file.insert_vol("/element_data", 0)
+        vol_file = h5_file.get_vol(path="/element_data")
+        # Set TensorComponent and ExtentsAndTensorVolumeData to
+        # be written
+        basis = ds.Legendre
+        quad = ds.Gauss
+        element_vol_obs1 = [
+            ds.ElementVolumeData([2, 2, 2], [
+                ds.TensorComponent(grid_names[0] + "/S",
+                                   tensor_components_and_coords[0]),
+                ds.TensorComponent(grid_names[0] + "/x-coord",
+                                   tensor_components_and_coords[1]),
+                ds.TensorComponent(grid_names[0] + "/y-coord",
+                                   tensor_components_and_coords[2]),
+                ds.TensorComponent(grid_names[0] + "/z-coord",
+                                   tensor_components_and_coords[3]),
+                ds.TensorComponent(grid_names[0] + "/T_x",
+                                   tensor_components_and_coords[4]),
+                ds.TensorComponent(grid_names[0] + "/T_y",
+                                   tensor_components_and_coords[5]),
+                ds.TensorComponent(grid_names[0] + "/T_z",
+                                   tensor_components_and_coords[6])
+            ], [basis, basis, basis], [quad, quad, quad])
+            for observation_value in observation_values
+        ]
+
+        element_vol_obs2 = [
+            ds.ElementVolumeData([2, 2, 2], [
+                ds.TensorComponent(grid_names[1] + "/S",
+                                   tensor_components_and_coords[0]),
+                ds.TensorComponent(grid_names[1] + "/x-coord",
+                                   tensor_components_and_coords[2]),
+                ds.TensorComponent(grid_names[1] + "/y-coord",
+                                   tensor_components_and_coords[3]),
+                ds.TensorComponent(grid_names[1] + "/z-coord",
+                                   tensor_components_and_coords[1]),
+                ds.TensorComponent(grid_names[1] + "/T_x",
+                                   tensor_components_and_coords[6]),
+                ds.TensorComponent(grid_names[1] + "/T_y",
+                                   tensor_components_and_coords[4]),
+                ds.TensorComponent(grid_names[1] + "/T_z",
+                                   tensor_components_and_coords[5])
+            ], [basis, basis, basis], [quad, quad, quad])
+            for observation_value in observation_values
+        ]
+        # Write extents and tensor volume data to volfile
+        vol_file.write_volume_data(observation_ids[0], observation_values[0],
+                                   [element_vol_obs1[0], element_vol_obs2[0]])
+        vol_file.write_volume_data(observation_ids[1], observation_values[1],
+                                   [element_vol_obs1[1], element_vol_obs2[1]])
+        self.h5_file_r = h5_file
+
     def tearDown(self):
         if os.path.isfile(self.file_name_w):
             os.remove(self.file_name_w)
@@ -81,6 +151,13 @@ class TestIOH5VolumeData(unittest.TestCase):
         extents = vol_file.get_extents(observation_id=obs_id)
         expected_extents = [[2, 2, 2]]
         self.assertEqual(extents, expected_extents)
+        bases = vol_file.get_bases(observation_id=obs_id)
+        expected_bases = [["Legendre", "Legendre", "Legendre"],
+                          ["Legendre", "Legendre", "Legendre"]]
+        self.assertEqual(bases, expected_bases)
+        quadratures = vol_file.get_quadratures(observation_id=obs_id)
+        expected_quadratures = [["Gauss", "Gauss", "Gauss"],
+                                ["Gauss", "Gauss", "Gauss"]]
         h5_file.close()
 
     # Test that the tensor components, and tensor data  are retrieved correctly
