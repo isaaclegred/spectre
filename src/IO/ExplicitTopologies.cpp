c34c1e6bfc8fd44cc45dcb5afcd759247a164ba2
Isaac Legred <inl7@cornell.edu>
2020-07-16 13:36:07 -0700

    Implement two sphere compute_cells

--- src/IO/ExplicitTopologies.cpp
+++ src/IO/ExplicitTopologies.cpp
@@ -11,9 +11,14 @@
 #include "Connectivity.hpp"
 
 namespace vis::detail {
+std::vector<CellInTopology> glue_along_dimension(
+    std::vector<CellInTopology> input) {}
 
 std::vector<CellInTopolgy> Sphere<1>::compute_cells() {
   size_t num_points = extents[0];
+  if (num_points < 2) {
+    ERROR("Constructing a 1-sphere requires at least 2 points")
+  }
   auto line_cells = compute_cells(num_points);
   // Attach the two loose ends of a line
   basic_cells.emplace_back(BasicTopology::Line,
@@ -24,9 +29,50 @@ std::vector<CellInTopolgy> Sphere<1>::compute_cells() {
 std::vector<CellInTopolgy> Sphere<2>::compute_cells() {
   size_t theta_pts = extents[0];
   size_t phi_pts = extents[1];
-  auto square_cells = compute_cells(theta_pts, phi_pts);
+  if (theta_pts < 2 or phi_pts < 2) {
+    ERROR("2-sphere requires at least 2 theta points and 2 phi points")
+  }
+  std::vector<CellInTopology> result;
+  //
+  result.reserve((theta_pts - 1) * (phi_pts))
+      // Two distinguished points
+      size_t south_pole = 0;
+  size_t north_pole = theta_pts * phi_pts - 1;  // is at least 3
   // Glue the theta points together with squares, and collapse the phi points
-  // together at poles
+  // together at poles, first cells varies more slowly then second cells
+  size_t points_so_far = 0;
+  for (size_t theta_pt = 0; theta_pt < theta_pts; theta_pt++) {
+    if (theta_pt = south_pole) {
+      for (size_t phi_pt = 0; phi_pt < phi_pts; phi_pt++) {
+        // Add polar wedges
+        result.emplace_back(BasicTopology::Quad,
+                            {south_pole, south_pole + phi_pt,
+                             south_pole + phi_pt + 1, south_pole})
+      }
+      points_so_far += 1;
+    }
+    // The level below the north pole
+    else if (theta_pt = theta_pts - 1) {
+      // Add polar wedges
+      result.emplace_back(BasicTopology::Quad,
+                          {north_pole, north_pole - phi_pt,
+                           north_pole - phi_pt + 1, north_pole})
+    } else {  // Neither of the pole special cases
+      for (size_t phi_pt = 0; phi_pt < phi_pts; theta_pt++) {
+        // Add the square which is up and to the right of this point
+        result.emplace_back(
+            BasicTopology::Quad,
+            {
+                points_so_far + phi_pt,                // Bottom left
+                points_so_far + phi_pt + 1,            // Bottom right
+                points_so_far + phi_pt + phi_pts + 1,  // Upper right
+                point_so_far + phi_pt + phi_pts        // Upper left
+            })
+      }
+      points_so_far += phi_pts;
+    }
+  }
+  return result;
 }
 
 }  // namespace vis::detail
