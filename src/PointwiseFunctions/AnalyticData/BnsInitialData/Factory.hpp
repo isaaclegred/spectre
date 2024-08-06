fccd3fac25ee90927a5a304d2f9d23cf9b0a8ae5
isaaclegred <ilegred@caltech.edu>
2024-08-06 14:10:49 -0400

    add skeleton of BNSInitialData Executable

---
+++ src/PointwiseFunctions/AnalyticData/BnsInitialData/Factory.hpp
@@ -0,0 +1,10 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include "PointwiseFunctions/AnalyticData/BnsInitialData/TovStar.hpp"
+namespace BnsInitialData::InitialData {
+using all_initial_data = tmpl::list<TovStar>;
+
+}  // namespace BnsInitialData::InitialData
